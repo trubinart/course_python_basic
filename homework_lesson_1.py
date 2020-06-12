8ce10558f7c38999b41db3fbac5039cdcfcd1d89
truart <trubinart@gmail.com>
2020-06-12 20:21:09 +0300

    task 1 - done / task 2 - rewrite / task 3 - done

--- homework_lesson_1.py
+++ homework_lesson_1.py
@@ -1,27 +1,34 @@
 # ЗАДАНИЕ 1
-my_var_1 = 1
-my_var_2 = 2
-print(my_var_1, my_var_2)
-
-var_from_user_num = input('Введите число: ')
-var_from_user_str = input('Введите текст: ')
-
-var_from_user_num = int(var_from_user_num)
-
-print(var_from_user_num)
-print(var_from_user_num)
-print(var_from_user_str)
+# my_var_1 = 1
+# my_var_2 = 2
+# print(my_var_1, my_var_2)
+#
+# var_from_user_num = input('Введите число: ')
+# var_from_user_str = input('Введите текст: ')
+#
+# var_from_user_num = int(var_from_user_num)
+#
+# print(var_from_user_num)
+# print(var_from_user_num)
+# print(var_from_user_str)
 
 # ЗАДАНИЕ 2
-user_ask_time = input('Введите число в секундах: ')
-while True:
-    if user_ask_time.isdigit():
-        user_ask_time = int(user_ask_time)
-        how_min_in_user_ask = int(user_ask_time / 60)
-        user_hour = int(how_min_in_user_ask // 60)
-        user_min = int(how_min_in_user_ask % 60)
-        user_sec = user_ask_time % 60
-        print(f'{user_hour} часов {user_min} минут {user_sec} секунд')
-        break
-    else:
-        user_ask_time = input('Вы ввели текст, а нужны секунды: ')
+# user_ask_time = input('Введите число в секундах: ')
+# while True:
+#     if user_ask_time.isdigit():
+#         user_ask_time = int(user_ask_time)
+#         hour = user_ask_time // 3600
+#         min = int(user_ask_time % 3600 /60)
+#         sec = user_ask_time % 60
+#         print(f'{hour} час : {min} мин : {sec} сек')
+#         break
+#     else:
+#         user_ask_time = input('Вы ввели текст, а нужны секунды: ')
+
+# ЗАДАНИЕ 3
+# user_ask_number = input('Введите любое число: ')
+# sum = int(user_ask_number) + int(user_ask_number*2) + int(user_ask_number*3)
+# print(sum)
+
+
+
