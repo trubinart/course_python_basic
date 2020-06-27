3bda134a56726884f10d2f0c28a1b43d4ecbef25
truart <trubinart@gmail.com>
2020-06-27 18:00:10 +0300

    task 2 - done

---
+++ les5_task_2.py
@@ -0,0 +1,12 @@
+
+with open('file_for_task_3.txt', 'r', encoding='UTF-8') as file:
+    num = 1
+    for i in file:
+        list = i.split()
+        result = len(list)
+        print(f'В строке {num} - {result} слов')
+        num += 1
+
+with open('file_for_task_3.txt', 'r', encoding='UTF-8') as file:
+    count_line = len(file.readlines())
+    print(f'В файле всего {count_line} строк')
