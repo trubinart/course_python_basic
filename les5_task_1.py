a0f5ad3d28de960debc341f82710f827993f4379
truart <trubinart@gmail.com>
2020-06-28 09:28:46 +0300

    correct task 1

--- les5_task_1.py
+++ les5_task_1.py
@@ -1,11 +1,11 @@
 
 user_ask = input('Введите строку для записи в файл: ')
-with open('file_task_1.txt', 'x', encoding='UTF-8') as file:
-    file.write(f'{user_ask}\n')
+file = open('file_task_1.txt', 'x', encoding='UTF-8')
+file.write(f'{user_ask}\n')
 
 while user_ask:
     user_ask = input('Может хотите еще что-то записать?: ')
-    with open('file_task_1.txt', 'a', encoding='UTF-8') as file:
-        file.write(f'{user_ask}\n')
+    file.write(f'{user_ask}\n')
 else:
     print('Круто, файл создан и вы можете его прочитать!')
+file.close()
